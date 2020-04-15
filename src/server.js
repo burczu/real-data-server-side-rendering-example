67939d3f15465e79dd49b5dca50a17803c04c6b6
Bartek Dybowski <b .dybowski@gmail.com>
2020-04-15 20:09:31 +0200

    servers-side changes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import path from 'path';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import axios from 'axios';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 import { StaticRouter } from 'react-router';
@@ -17,9 +19,18 @@ app.use(express.static(path.join(__dirname)));
 app.get('*', async (req, res) => {
   const scripts = ['vendor.js', 'client.js'];
 
-  const initialState = { initialText: 'rendered on the server' };
+  let initialState;
+  if (req.url === '/') {
+    const repositories = await axios.get('https://api.github.com/repositories');
+    initialState = {
+      repositoryNames: repositories.data.map((repo) => ({
+        id: repo.id,
+        name: repo.name,
+      })),
+    };
+  }
 
-  const store = createStore(reducers, initialState);
+  const store = createStore(reducers, initialState, applyMiddleware(thunk));
 
   const appMarkup = ReactDOMServer.renderToString(
     <StaticRouter location={req.url} context={{}}>
